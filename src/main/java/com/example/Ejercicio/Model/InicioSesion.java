ce93209f46812579d974445e0c429a8043e0ae25
SHUBAAA <m.alvial03@ufromail.cl>
2022-09-28 15:42:10 -0300

    Final de Prototipo

--- src/main/java/com/example/Ejercicio/Model/InicioSesion.java
+++ src/main/java/com/example/Ejercicio/Model/InicioSesion.java
@@ -1,5 +1,7 @@
 package com.example.Ejercicio.Model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 /**
@@ -11,9 +13,12 @@ import javax.persistence.*;
  * @author Manuel Alvial
  */
 @Entity
+@Data
 public class InicioSesion {
 
+
     @Id
+
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(nullable = false)
@@ -34,35 +39,4 @@ public class InicioSesion {
         this.correo = correo;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombreuser() {
-        return nombreuser;
-    }
-
-    public void setNombreuser(String nombreuser) {
-        this.nombreuser = nombreuser;
-    }
-
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
 }
