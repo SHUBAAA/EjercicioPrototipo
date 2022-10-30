b6001a57e58276ce58439c944ad19b97206828c4
SHUBAAA <m.alvial03@ufromail.cl>
2022-10-30 19:06:06 -0300

    Pasamos a nuevo git

---
+++ src/main/java/com/example/Ejercicio/Repository/RepoBoleta.java
@@ -0,0 +1,7 @@
+package com.example.Ejercicio.Repository;
+
+import com.example.Ejercicio.Model.Boleta;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepoBoleta extends CrudRepository<Boleta,Integer> {
+}
